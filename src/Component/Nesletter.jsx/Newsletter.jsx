cc592773798632eb2e925442c9135f8e3b3f185d
its-awais <awaisahmed123334444@gmail.com>
2024-12-12 19:49:18 +0500

    third commit

--- src/Component/Nesletter.jsx/Newsletter.jsx
+++ src/Component/Nesletter.jsx/Newsletter.jsx
@@ -27,21 +27,21 @@ const Newsletter = () => {
   return (
     <>
     
-     <div className="newsletterForm">
+     <main className="newsletterForm">
        <div className="formSubmit ms-4">
         <h1 className="fw-bold fs-1">Stay updated!</h1>
         <p className="newsletter Updates">Join 60000+ product managers receiving monthly updates on</p>
          <div className="deleviry manger">
             <div className="first deleviry mb-2">
-         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
+         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 64 64"><defs><linearGradient id="gradient-1" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#gradient-1)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
          <p className="deliveries">product discovery and building what matters</p>
          </div>
          <div className="second deleviry  mb-2">
-         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
+         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 64 64"><defs><linearGradient id="gradient-2" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#gradient-2)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
          <p className="deliveries">Measuring to ensure updates are succesfull</p>
          </div>
          <div className="third deleviry  mb-2">
-         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
+         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 64 64"><defs><linearGradient id="gradient-3" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#gradient-3)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
          <p className="deliveries">Add much more!</p>
          </div>
          </div>
@@ -57,7 +57,7 @@ const Newsletter = () => {
        <div className="newsletter-img">
         <img src={newsletterImage} alt="Newsletter illustration"></img>
        </div>
-     </div>
+     </main>
     </>
   )
 }
